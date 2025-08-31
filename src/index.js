555e15e109eaf5a7d3ad3b2b838050c06a85a553
Aman Kumar <ar1075840@gmail.com>
2025-09-01 00:01:55 +0530

    controller and postman

--- src/index.js
+++ src/index.js
@@ -1,23 +1,20 @@
-//require('dotenv').config({path:'./env'})
-import dotenv from 'dotenv'
-import connectDB from './db/index.js'
-
+//("dotenv").config({ path: "./env" });
+import dotenv from "dotenv";
+import connectDB from "./db/index.js";
+import { app } from "./app.js";
 dotenv.config({
-    path:'/.env'
-})
+  path: "/.env",
+});
 
 connectDB()
-
-.then(()=>{
-    app.listen(process.env.PORT ||8000 , ()=>{
-        console.log(` Server is runnign at port: ${process.env.PORT}`);
-        
-    })
-})
-.catch((err)=>{
-    console.log("MONGODB connection failed  ",err);
-    
-})
+  .then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(` Server is running at port: ${process.env.PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log("MONGODB connection failed  ", err);
+  });
 
 
 
@@ -30,7 +27,12 @@ connectDB()
 
 
 
-// one method 
+
+
+
+
+  
+// one method
 
 /*
 import express from "express"
